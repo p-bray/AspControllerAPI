4d9db0a488c6647fca1667a148dd0a49f733e787
p-bray <pbr23869@gmail.com>
2024-10-11 22:05:13 -0500

    just some comments. Not much time to program today

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -24,6 +24,7 @@ namespace AspControllerAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
+            //The List gets are simple. All you do is an async call to the ToListAsync() method. 
             return await _context.TodoItems.ToListAsync();
         }
 
@@ -31,13 +32,16 @@ namespace AspControllerAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
         {
+            //placeholder for the results of the FindAsync(id) method. Really just a SELECT query
             var todoItem = await _context.TodoItems.FindAsync(id);
 
+            //Make sure we found something that matches the Id passed
             if (todoItem == null)
             {
                 return NotFound();
             }
 
+            //return the dang thing
             return todoItem;
         }
 
@@ -46,13 +50,17 @@ namespace AspControllerAPI.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
         {
+            //Make sure the id of the new object and the target id match
             if (id != todoItem.Id)
             {
                 return BadRequest();
             }
 
+            //Set the entry in the list of EntityEntries to have the Modified State enum. 
+            //This finds the entity in the Db, changes it to match the passed entity's properties
             _context.Entry(todoItem).State = EntityState.Modified;
 
+            //Concurrency protection
             try
             {
                 await _context.SaveChangesAsync();
@@ -99,6 +107,7 @@ namespace AspControllerAPI.Controllers
             return NoContent();
         }
 
+        //Helper method to check the Db table for any instances of the entry with passed id. Just to make sure it exists. 
         private bool TodoItemExists(int id)
         {
             return _context.TodoItems.Any(e => e.Id == id);
